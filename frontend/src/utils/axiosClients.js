fa0be3373721cc47bb165e1eb6f2c150651510c0
nishthapriya1 <nishtha1priya@gmail.com>
2025-07-28 18:29:42 +0530

    Update axiosClients.js

--- frontend/src/utils/axiosClients.js
+++ frontend/src/utils/axiosClients.js
@@ -1,13 +1,20 @@
-import axios from "axios"
+// import axios from "axios"
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:5000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+// const axiosClient =  axios.create({
+//     baseURL: 'http://localhost:5000',
+//     withCredentials: true,
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// });
+
+
+// export default axiosClient;
+
+const axiosClient = axios.create({
+  baseURL: 'https://your-backend-name.vercel.app', // replace with actual URL
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
-
-
-export default axiosClient;
-
