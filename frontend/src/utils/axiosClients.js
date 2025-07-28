6b4ea8374de0962162065fe85e53fb9a4a9732a7
nishthapriya1 <nishtha1priya@gmail.com>
2025-07-28 18:37:42 +0530

    Update axiosClients.js

--- frontend/src/utils/axiosClients.js
+++ frontend/src/utils/axiosClients.js
@@ -1,18 +1,5 @@
-// import axios from "axios"
-
-// const axiosClient =  axios.create({
-//     baseURL: 'http://localhost:5000',
-//     withCredentials: true,
-//     headers: {
-//         'Content-Type': 'application/json'
-//     }
-// });
-
-
-// export default axiosClient;
-
 const axiosClient = axios.create({
-  baseURL: 'https://your-backend-name.vercel.app', // replace with actual URL
+  baseURL: 'https://smartstocker1.vercel.app/', 
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
